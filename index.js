2b0aa88bf61ed4d61ceda6b895fab104218ebe70
shaikot07 <saifulislamshaikat007@gmail.com>
2023-10-19 15:08:26 +0600

    added get operation for put operation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -44,6 +44,20 @@ async function run() {
             const result= await productCollection.insertOne(newProduct);
             res.send(result);
       })
+
+      // step: 3 
+
+      app.get('/product/:id',async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await productCollection.findOne(query);
+        res.send(result)
+      })
+      // app.put('/product/:id', async(req, res)=>{
+      //       const id = req.params.id;
+      //       const filter= {_id: new ObjectId(id)};
+      //       const options = { upsert: true };
+      // })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
