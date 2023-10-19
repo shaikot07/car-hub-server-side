0f49f202af315161fdfe6e9751c48d90b6be47a3
shaikot07 <saifulislamshaikat007@gmail.com>
2023-10-19 17:13:54 +0600

    added cartCollection post operation

--- index.js
+++ index.js
@@ -27,68 +27,83 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-      
-    const productCollection=client.db('productDB').collection('product');
+
+    const productCollection = client.db('productDB').collection('product');
+    const cartCollection = client.db('productDB').collection('cart');
 
 
 
-    app.get('/product',async(req,res)=>{
-      const cursor=productCollection.find();
-      const result= await cursor.toArray()
+    app.get('/product', async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray()
       res.send(result)
     })
-      // step:1 
-      app.post('/product',async(req,res)=>{
-            const newProduct=req.body;
-            console.log(newProduct);
-            const result= await productCollection.insertOne(newProduct);
-            res.send(result);
-      })
+    // step:1 
+    app.post('/product', async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    })
 
-      // step: 3 
+    // step: 3 
 
-      app.get('/product/:id',async(req, res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await productCollection.findOne(query);
-        res.send(result)
-      })
-      app.put('/product/:id', async(req, res)=>{
-            const id = req.params.id;
-            const filter= {_id: new ObjectId(id)};
-            const options = { upsert: true };
-            const updateProduct =req.body;
-            const product = {
-              $set:{
-                image:updateProduct.image,
-                name:updateProduct.name,
-                brandName:updateProduct.brandName,
-                type:updateProduct.type,
-                price:updateProduct.price,
-                shortDescription:updateProduct.shortDescription,
-                rating:updateProduct.rating
-              }
-            }
-          const result = await productCollection.updateOne(filter,product, options);
-          res.send(result);
-      })
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result)
+    })
+    app.put('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateProduct = req.body;
+      const product = {
+        $set: {
+          image: updateProduct.image,
+          name: updateProduct.name,
+          brandName: updateProduct.brandName,
+          type: updateProduct.type,
+          price: updateProduct.price,
+          shortDescription: updateProduct.shortDescription,
+          rating: updateProduct.rating
+        }
+      }
+      const result = await productCollection.updateOne(filter, product, options);
+      res.send(result);
+    })
+
+    app.post('/cart', async (req, res) => {
+      const addToCart = req.body;
+      console.log(addToCart);
+      const result = await cartCollection.insertOne(addToCart);
+      res.send(result);
+    })
+
+
+    app.get('/cart', async (req, res) => {
+      const cursor = cartCollection.find();
+      const result = await cursor.toArray()
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-//     await client.close();
+    //     await client.close();
   }
 }
 run().catch(console.dir);
 
 
-app.get('/',(req, res)=>{
-      res.send('simple CRUD is RUNNING')
+app.get('/', (req, res) => {
+  res.send('simple CRUD is RUNNING')
 })
 
-app.listen(port,()=>{
-      console.log(`simple CRUD is running on port,${port}`);
+app.listen(port, () => {
+  console.log(`simple CRUD is running on port,${port}`);
 })
 
