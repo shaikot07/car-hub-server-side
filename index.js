41a453640c2b9fbecb71ec7a2c855e83a11a3860
shaikot07 <saifulislamshaikat007@gmail.com>
2023-10-19 23:37:42 +0600

    added delete method

--- index.js
+++ index.js
@@ -88,6 +88,17 @@ async function run() {
       res.send(result)
     })
 
+
+    app.delete('/cart/:id', async(req, res)=>{
+      const id = req.params.id;
+      console.log(id); //id value is oky 
+      const query = {_id: new ObjectId(id) };
+      const result = cartCollection.deleteOne(query);
+      console.log(result);
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
